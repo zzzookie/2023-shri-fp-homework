290f25df787b7347bbc1f0a27f7b6a02276b44c6
Ivan Shulgin <multizookie@gmail.com>
2023-07-21 12:42:48 +0700

    Первая часть почти сделана, вторая - доделана

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,75 @@
-/**
- * @file Домашка по FP ч. 2
- *
- * Подсказки:
- * Метод get у инстанса Api – каррированый
- * GET / https://animals.tech/{id}
- *
- * GET / https://api.tech/numbers/base
- * params:
- * – number [Int] – число
- * – from [Int] – из какой системы счисления
- * – to [Int] – в какую систему счисления
- *
- * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
- */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import { tap, pipe, curry, allPass, gt, lt, equals, pipeWith, ifElse } from 'ramda'
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const isPositive = value => gt(value, 0)
+const longerThan2 = value => gt(value.length, 2)
+const shorterThan10 = value => lt(value.length, 10)
+const isNumber = value => equals(typeof Number(value), 'number')
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const validate = (string) =>
+  allPass([isPositive, longerThan2, shorterThan10, isNumber])(string)
+    ? string
+    : 'ValidationError'
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const round = async value => Math.round(Number(value))
+const lengthCount = async string => string.length
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const getSquare = async value => value ** 2;
+const getRemainder = async value => value % 3;
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const getApiResult = api => async value => {
+  const apiAnswer = await api(value)
+  const answer = await apiAnswer
+  return answer.result
+  // function retry() {
+  //   try {
+  //     result = answer.result
+  //     return answer
+  //   } catch (error) {
+      
+  //   }
+  // }
+}
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const queryBinaryValue = async number => await api.get('https://api.tech/numbers/base', { number, from: 10, to: 2 })
+const queryAnimalName = async id => api.get(`https://animals.tech/${id}`, {});
+
+const getBinaryValue = getApiResult(queryBinaryValue)
+const getAnimalName = getApiResult(queryAnimalName)
+
+const processSequence = async ({value, writeLog, handleSuccess, handleError}) => {
+  const log = curry(tap)(writeLog)
+  const error = curry(tap)(handleError)
+
+  const pipeAsyncLogged = (fns) => {
+    return pipeWith(async (f, prevRes) => {
+      const value = await prevRes
+      Number(value) && log(value)
+      return f(value)
+    })(fns)
+  };
+
+  const getAnimal = pipeAsyncLogged([
+    round,
+    getBinaryValue,
+    lengthCount,
+    getSquare,
+    getRemainder,
+    getAnimalName,
+    handleSuccess
+  ])
+  
+  pipe(
+    log,
+    validate,
+    ifElse(
+      equals('ValidationError'),
+      error,
+      getAnimal
+    )
+  )(value)
+}
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -12,39 +12,60 @@
  *
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
+import { allPass, anyPass, prop } from 'ramda'
+
+const isSameColor = color => shapesStrOrObj => {
+  const shapes = typeof shapesStrOrObj === 'string'
+    ? [shapesStrOrObj]
+    : Object.values(shapesStrOrObj)
+  return shapes.every(shape => shape === color)
+}
+
+const countColor = color => shapesObj => Object.values(shapesObj).filter(shape => shape === color).length
+
+const getTriangle = prop('triangle')
+const getCircle = prop('circle')
+const getSquare = prop('square')
+const getStar = prop('star')
+
+const isWhite = isSameColor('white')
+const isOrange = isSameColor('orange')
+const isGreen = isSameColor('green')
+const isBlue = isSameColor('blue') 
+const isRed = isSameColor('red')
+
+const countWhite = countColor('white')
+const countOrange = countColor('orange')
+const countGreen = countColor('green')
+const countBlue = countColor('blue') 
+const countRed = countColor('red')
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
-
-    return star === 'red' && square === 'green';
-};
+export const validateFieldN1 = ({ circle, square, triangle, star }) => isRed(star) && isGreen(square) && isWhite({triangle, circle})
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = (figs) => countGreen(figs) >= 2;
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (figs) => countRed(figs) === countBlue(figs)
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = ({ circle, square, star }) => isBlue(circle) && isRed(star) && isOrange(square);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = (figs) => Math.max(countGreen(figs), countRed(figs), countBlue(figs), countOrange(figs)) >= 3;
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = (figs) => countGreen(figs) === 2 && countRed(figs) === 1 && isGreen(figs.triangle)
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = (figs) => isOrange(figs);
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = ({ star }) => !(isRed(star) || isWhite(star));
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = (shapes) => isGreen(shapes);
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = ({ square, triangle }) => triangle === square && !isWhite(triangle);
